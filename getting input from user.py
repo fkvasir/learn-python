8b357447952cc83ab75fd07b15b42ae2f45f2a5a
fkvasir <90448551+fkvasir@users.noreply.github.com>
2021-09-16 23:51:03 +0800

    Add files via upload

---
+++ getting input from user.py
@@ -0,0 +1,7 @@
+name = input("ENTER YOUR NAME: ")
+print("Hello " + name + "!")
+
+name = input("ENTER YOUR NAME:")
+age = input("ENTER YOUR AGE:")
+print("Hello! So you are " + name + "!")
+print("Your age is" + "? " + age)
