4d40839789f806119b78ecd8f789afcc61c096a3
Fulgent Kvasir Lavesores <90448551+fkvasir@users.noreply.github.com>
2021-09-19 00:56:44 +0800

    added a commit

---
+++ mad libs game.py
@@ -0,0 +1,17 @@
+
+color = input("Enter a color: ")
+flower = input("Enter a name of a flower: ")
+curse_word = input("Enter a Filipino curse word: ")
+
+print("Roses are " + color)
+print( flower + " are blue")
+print("Edi " + curse_word + " mo")
+
+breaker = "for more examples"
+def paragraph_breaker(yeah):
+  print(yeah.upper())
+paragraph_breaker(breaker)
+landform = input("ENTER A TYPE OF NATURE BEAUTY FOUND IN LAND: ")
+positive = input("ENTER THE OPPOSITE TERM FOR DESPAIR: ")
+print("From a " + landform + " of despair")
+print("A stone of " + positive)
