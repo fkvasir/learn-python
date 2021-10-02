73cb7310179eccfc041ac971446ec4c0c8c3808c
Fulgent Kvasir Lavesores <90448551+fkvasir@users.noreply.github.com>
2021-10-02 22:50:26 +0800

    Add files via upload

---
+++ return statements.py
@@ -0,0 +1,12 @@
+def cubeof(number):
+  return number*number*number
+
+print(cubeof(5))
+# Use return whenever we want to receive the information from a function
+# return a value back to the caller
+
+def square(num):
+  return num*num
+result = square(9)
+print(result)
+# getting information back from the function
