a4cb258591d6b63f5b5b4a8923845225ba6948ff
Fulgent Kvasir Lavesores <90448551+fkvasir@users.noreply.github.com>
2021-10-02 21:59:30 +0800

    Add files via upload

---
+++ functions.py
@@ -0,0 +1,27 @@
+def my_petdogs(dogs):
+  for x in dogs:
+    print(x)   
+dogs = ["Lino", "Kirvy", "Babi"]
+my_petdogs(dogs)
+
+def hi(dada):
+  print(dada)
+
+hi("hello")
+
+def my_functions(places):
+  print("The places that I want to visit to: " + places)
+
+my_functions("Switzerland")
+my_functions("England")
+my_functions("Japan")
+
+def hi():
+  print("Hello mina-san")
+
+hi()
+
+def say_hi(name):
+  print("Hello " + name)
+say_hi("Douglas")
+say_hi("Mc Arthur")
