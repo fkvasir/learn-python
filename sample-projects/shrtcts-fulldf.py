506f3cbbf8fcbfe1bcc8bb399b51efadd9656872
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-11-05 21:05:29 +0800

    updates

---
+++ sample-projects/shrtcts-fulldf.py
@@ -0,0 +1,30 @@
+# // sample applications for "DICTIONARY"//
+monthConversions = {
+  "Jan": "January", # --> /keys should be unique/
+  "Feb": "February",
+  "Mar": "March",
+  "Apr": "April",
+  "May": "May",
+  "Jun": "June",
+  "Jul": "July",
+  "Aug": "August",
+  "Sep": "September",
+  "Nov": "November",
+  "Dec": "December"
+
+}
+
+
+
+print(monthConversions["Nov"])
+print(monthConversions["Aug"])
+print(monthConversions["Feb"])
+print(monthConversions["Jan"])
+print(monthConversions.get("Dec"))
+print(monthConversions.get("Sep"))
+print(monthConversions.get("Love")) # //not included in the key//
+print(monthConversions.get("Love", "Invalid key")) # //default value// 
+print(monthConversions.get("Love", "Not a valid key")) # //default value// 
+
+
+
