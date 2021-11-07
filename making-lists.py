cf79a44ee8bb311ed14fc20ebc382f7a621aedcf
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-11-07 22:07:20 +0800

    added changes

--- making-lists.py
+++ making-lists.py
@@ -7,13 +7,13 @@ def foods_I_love(desserts):
 
 desserts = ["leche flan", "mango float", "chocolate cake"]
 foods_I_love(desserts)
-
+print("_________________\n")
 
 # Just a normal LIST
 friends = ["Minju", 2 , "Tzuyu"]
 print(friends)
 
-
+print("___________________\n")
 # Referring elements by their index
 
 friends = ["Minju", "Nagyung" , "Tzuyu"] # in coding, always start from zero 
@@ -27,17 +27,22 @@ print(friends[-1])
 print(friends[-2])
 print(friends[0])
 print(friends[1])
-
+print("_________________________________\n")
 # If I want to print all of the elements but except the first then:
+print("Printing-all-elements-except-one")
 print(friends[1:])
 print(friends[2:])
+print("___________________________\n")
 
 # In specifying a range
+print("Specifying range")
 close = ["Minju", "Nagyung", "Tzuyu", "Lisa", "Meru"]
 print(close[1:4])
 print(close[3:4])
 
+print("_____________________________\n")
 # Modifying element
+print("Modifying element")
 close[1] = "Minjoo"
 print(close[1])
 # Basically just changing one value of an element without changing others
--- while-loops.py
+++ while-loops.py
@@ -1,1 +1,9 @@
- 
+i = 1
+"""
+while (statement) --->/as-long-as-true/
+"""
+while i <= 10:
+  print(i)
+  i += 1
+  
+print ("Done with loop")
