d5c07f7f06345d0a99ca6f67525a61b22cf998bd
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-11-09 14:43:14 +0800

    added changes

--- while-loops.py
+++ while-loops.py
@@ -7,3 +7,18 @@ while i <= 10:
   i += 1
   
 print ("Done with loop")
+
+print("~~~~~~~~~~~~~~~~\n")
+
+
+
+x = int(input("Enter a number in cm\n-> "))
+y = int(input("Input a second larger number\n-> "))
+CM_to_Inches = 0.3937
+g = 0
+while g <= 11:
+  if y == x + 10:
+    print(x ,  (x+g) * CM_to_Inches)
+  else:
+    print("\nError: Invalid Intervals\n")
+  g += 11
