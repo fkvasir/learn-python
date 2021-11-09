d046c6072046935c1b6df5003c02e1db0d9c1ec3
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-11-09 15:17:44 +0800

    added changes

--- while-loops.py
+++ while-loops.py
@@ -11,7 +11,7 @@ print ("Done with loop")
 print("~~~~~~~~~~~~~~~~\n")
 
 
-
+print("Input a number in centimeter to be converted to inches\nShould be a 10-interval number from the first and second number")
 x = int(input("Enter a number in cm\n-> "))
 y = int(input("Input a second larger number\n-> "))
 CM_to_Inches = 0.3937
