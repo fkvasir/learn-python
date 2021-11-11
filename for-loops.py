bcda9a170a579df63b77a4618744370dac142b82
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-11-12 00:55:59 +0800

    added changes

---
+++ for-loops.py
@@ -0,0 +1,37 @@
+print("\n~~~~~~~~~~~~~~")
+family = ["Laura", "Caridad", "Hamsel"]
+for member in family:
+  print(member)
+print("\n~~~~~~~~~~\n")
+for letter in "Kvasir":
+  print(letter)
+print("\n~~~~~~~~~~~\n")
+crhs = ["Shyne", "Cthrn", "Nrhmd", "Dryl"]
+for cruhsi in crhs:
+  print(cruhsi)
+print("\n~~~~~~~~~\n")
+for index in range(7):
+  print(index)
+print("\n~~~~~~~~~\n")
+for index in range(len(crhs)): #the-same-as-the-first-but-w/range
+  print(crhs[index])
+print("\n")
+for cruhsi in crhs:
+  if cruhsi == "Dryl":
+    print("-since g11")
+  elif cruhsi == "Nrhmd":
+    print("-since g11(beginning)")
+  elif cruhsi == "Cthrn":
+    print("-since g10(summer)")
+  elif cruhsi == "Shyn":
+    print("-since g10")
+  print(cruhsi)
+print("\n~~~~~~~~~\n")
+for index in crhs:
+  if index == 0:
+    print("firstIteration")
+  if index == 1:
+    print("secondIteration")
+  else:
+    print("lastIteration")
+
