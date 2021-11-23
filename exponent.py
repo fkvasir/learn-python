d48195119f128bb363918af45e7c17c08e80e267
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-11-23 23:14:07 +0800

    added changes

--- exponent.py
+++ exponent.py
@@ -6,4 +6,8 @@ def to_thepower(base_num, power_num):
     result = result * base_num
   return result
 
-print(to_thepower(3,2))
+print(to_thepower(3, 2))
+print(to_thepower(3, 4))
+print(to_thepower(2, 3))
+print(to_thepower(10, 2))
+print(to_thepower(3, 0))
