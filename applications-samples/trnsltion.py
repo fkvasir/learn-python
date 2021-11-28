8d6c4c49b34579aa73ee63e497090f67ccd6d33f
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-11-29 00:24:43 +0800

    added changes

--- applications-samples/trnsltion.py
+++ applications-samples/trnsltion.py
@@ -8,4 +8,15 @@ def trnsltr(phrase):
       translation = translation + letter
   return translation
 print(trnsltr(input("Enter a word =>")))
-
+def trnslte(phrse):
+  translation = ""
+  for letter in phrse:
+    if letter.upper() in "AEIOU":
+      if letter.isupper():
+        translation = translation + "Eow"
+      else:
+        translation = translation + "eow"
+    else:
+      translation = translation + letter
+  return translation
+print(trnslte(input("Enter a word =>")))
