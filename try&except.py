cb6e1f9a31b5535f31eaafa100f30b7fdc79031b
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-12-07 00:02:18 +0800

    added changes

--- try&except.py
+++ try&except.py
@@ -1,11 +1,11 @@
 try:
   x = int(input("Enter a number in cm\n-> "))
   y = int(input("Input a second larger number\n-> "))
-  CM_to_Inches = 0.3937
+  assigned_value = 0.3937
   g = 0
   if y == x + 10:
     while g <= 10:
-      print(x ,  (x * CM_to_Inches))
+      print(x ,  (x * assigned_value))
       g += 1
       x += 1
   else:
