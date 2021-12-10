24021ccc49a898e7a6caa0c7b81c962ddde3aa00
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-12-10 23:34:46 +0800

    added changes

--- try&except.py
+++ try&except.py
@@ -14,3 +14,12 @@ except:
   print("INVALID")
 # /try-and-except-are-used-to-assign-what-to-do-for-expected-errors/
 # my-lazyass-starts-procrastinating
+try:
+  name = input("ENTER YOUR NAME:")
+  age = input("ENTER YOUR AGE:")
+  print("Hello! So you are " + name + "!")
+  print("Your age is" + "? " + age)
+  numb = int(input("Type any number of your choice: "))
+  print("Why did you choose " + numb + "as your number? Are you insane? Stupid idiot ")
+except:
+  print("Try again latur")
