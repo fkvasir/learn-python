c0d811d212ae9acb85c2558a62b0e9f2069b8eb6
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-12-10 23:37:53 +0800

    added changes

--- try&except.py
+++ try&except.py
@@ -23,3 +23,18 @@ try:
   print("Why did you choose " + numb + "as your number? Are you insane? Stupid idiot ")
 except:
   print("Try again latur")
+try:
+  first = int(input("Enter a number in inches(should be a 5 interval numbers)\nhere => "))
+  error = "ERROR: interval should be 5"
+  second = int(input("Enter second number where you want to stop converting\nhere => "))
+  Inches_to_CM = 2.53
+  f = 0
+  if second == first + 5:
+    while f <= 4:
+      print(first , (first * Inches_to_CM))
+      first += 1
+      f += 1
+  else:
+    print(error.upper())
+except:
+  print("okeh")
