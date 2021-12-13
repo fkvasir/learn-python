4af02eb7de93e3bb47504e1f7c7bb53ae89917c6
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-12-14 00:17:31 +0800

    added changes

--- read.py
+++ read.py
@@ -8,6 +8,9 @@ The sample above lets you open, read, write or append file.
 sample_file = open("file.txt", "r")
 
 print(sample_file.readline())
-print(sample_file.readline())
-
+print(sample_file.readlines())
+print(sample_file.readlines()[1])
+# readline automatically reads the first line of the file.
+# readlines automatically read all the lines of the file.
+# readlines with an indicator automatically reads the first line of the file without grouping.
 sample_file.close() 
