142ee8d469fd25996b77a20519ea07d1da027307
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-12-13 23:22:15 +0800

    added changes

--- read.py
+++ read.py
@@ -1,3 +1,7 @@
-open("file.txt", "r") # read
-open("file.txt", "w") # write
-open("file.txt", "a") # append - cant change anything in the file
+open("file.txt", "r")# read
+open("file.txt", "w")# write
+open("file.txt", "a")# append - cant change anything in the file but can add new information
+open("file.txt", "r+")# all the power of reading and writing
+"""
+
+"""
