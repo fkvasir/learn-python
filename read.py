f014aa3a9d5ab1099389ed1c9f0f74f1ac8363a8
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-12-13 23:33:34 +0800

    added changes

--- read.py
+++ read.py
@@ -3,5 +3,5 @@ open("file.txt", "w")# write
 open("file.txt", "a")# append - cant change anything in the file but can add new information
 open("file.txt", "r+")# all the power of reading and writing
 """
-
+The sample above lets you open, read, write or append file. 
 """
