12530e71ac21393c424a29414d6ff607e96c3ff3
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-12-29 23:48:43 +0800

    added changes

--- write.py
+++ write.py
@@ -1,3 +1,7 @@
-examplefile = open("file.txt", "a")
+examplefile = open("file.txt", "r")
 print(examplefile.read())
+examplefile.close()
+
+examplefile = open("file.txt", "a")
+examplefile.write("Michael - Physical Education")
 examplefile.close()
