b48f549629b7f385be9a482462a5ff2067115a9d
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2021-12-30 00:38:36 +0800

    added changes

--- write.py
+++ write.py
@@ -9,3 +9,13 @@ examplefile.close()
 examplefile = open("file.txt", "w") # /overwrite-the-existing-file/
 examplefile.write("\nKelly - Biology") # /erasing-the-existing-file-and-writing-a-new-one/
 examplefile.close()
+
+# CREATING NEW FILE
+examplefile = open("file.txt", "w") # /overwrite-the-existing-file/
+examplefile.write("\nKelly - Biology") # /erasing-the-existing-file-and-writing-a-new-one/
+examplefile.close()
+
+# WEBPAGE
+examplefile = open("file.html", "w") # /overwrite-the-existing-file/
+examplefile.write("<p> This is an HTML page</p>") # /erasing-the-existing-file-and-writing-a-new-one/
+examplefile.close()
