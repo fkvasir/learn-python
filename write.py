ac78e5fa2e83d0ea809e51cddc33acfa75c96493
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2022-01-09 23:30:35 +0800

    added changes

--- write.py
+++ write.py
@@ -1,17 +1,17 @@
-examplefile = open("file.txt", "r") # /only-reading-files/
+examplefile = open("file.txt", "r") # --> only-reading-files/
 print(examplefile.read())
 examplefile.close()
 
-examplefile = open("file.txt", "a") # /adding-some-other-data-to-the-existing-file/
+examplefile = open("file.txt", "a") # ---> adding-some-other-data-to-the-existing-file/
 examplefile.write("\nJelly - Biology")
 examplefile.close()
 
-examplefile = open("file.txt", "w") # /overwrite-the-existing-file/
+examplefile = open("file.txt", "w") # ---> overwrite-the-existing-file/
 examplefile.write("\nKelly - Biology") # /erasing-the-existing-file-and-writing-a-new-one/
 examplefile.close()
 
 # CREATING NEW FILE
-examplefile = open("file.txt", "w") # /overwrite-the-existing-file/
+examplefile = open("file.txt", "w") # ---> overwrite-the-existing-file/
 examplefile.write("\nKelly - Biology") # /erasing-the-existing-file-and-writing-a-new-one/
 examplefile.close()
 
