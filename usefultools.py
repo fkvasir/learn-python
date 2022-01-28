1a01209fe3b9483bc83a94280799b07228abd70c
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2022-01-28 20:56:01 +0800

    added changes

---
+++ usefultools.py
@@ -0,0 +1,11 @@
+import random
+
+feet_in_mile = 5280
+meters_in_km = 1000
+hello_venus = ["Alice","Nara","Jessica"]
+
+def get_file_text(filename):
+  return filename[filename.index(".") + 1:]
+
+def rolldice(num):
+  return random.randint(1,num)
