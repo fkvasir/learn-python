15aabcf4df96f4040ebac465fbd890d9c52821ea
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2022-01-28 21:06:53 +0800

    added changes

--- modules-pips.py
+++ modules-pips.py
@@ -3,4 +3,6 @@ import usefultools
 print(usefultools.rolldice(10))
 """
   >>> modules is any external python file that you want to use inside
- """
+  >>> You can find list of python modules in internet 
+  ... https://docs.python.org/3/py-modindex.html
+"""
