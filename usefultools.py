e04cd05a940cbbd40b27d865497a44c69b9ae78f
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2022-01-28 20:56:40 +0800

    added changes

--- usefultools.py
+++ usefultools.py
@@ -4,7 +4,7 @@ feet_in_mile = 5280
 meters_in_km = 1000
 hello_venus = ["Alice","Nara","Jessica"]
 
-def get_file_text(filename):
+def get_file_extension(filename):
   return filename[filename.index(".") + 1:]
 
 def rolldice(num):
