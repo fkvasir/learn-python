e452c1b73dbc7d6d1b64cb643d06777c7fcc6016
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2022-02-07 01:50:25 +0800

    added changes

--- app(student-class).py
+++ app(student-class).py
@@ -1,3 +1,11 @@
 from student import student
 
 student1 = student("Fulgent", "Computer Science", 1.7611, False)
+# >>> Access the datas from datatypes
+print(student1.GPA)
+"""
+... object is the data itself while class is what describes the datatype
+"""
+student2 = student("Catherine", "Business Management", None, False)
+print(student2.is_on_suspension)
+
