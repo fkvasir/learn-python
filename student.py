ec720f44eae3363f8e4c9d5b24faecf2802a95b0
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2022-02-07 01:54:34 +0800

    added changes

--- student.py
+++ student.py
@@ -1,6 +1,10 @@
-class student:
+class student: # overview of the *student*
 
-  def __init__(self, name, major, GPA, is_on_suspension):
+  def __init__(self, name, major, GPA, is_on_suspension): # initialyzing function # classifications
+    """
+    ... self.name -> the name of the student is stored in the name that we passed in
+    so on and so forth
+    """
     self.name = name
     self.major = major
     self.GPA = GPA
