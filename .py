72c2df9416caec892c1b9b3af754eb97785f8475
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2022-03-03 02:09:40 +0800

    added changes

---
+++ .py
@@ -0,0 +1,2 @@
+w = ord("y")
+print(w)
--- 1 ordinal.py
+++ 1 ordinal.py
@@ -1,14 +1,28 @@
 #No. 1
+
 name = ["L","y","n","n","i","e"]
-w = ord(name[0])
-x = ord(name[1])
-y = ord(name[2])
-z = ord(name[3])
-print(x)
+def ordinal():
+  w = ord(name[0])
+  x = ord(name[1])
+  y = ord(name[2])
+  z = ord(name[3])
+  return x, y
+
 
 """
 No. 2 - Compute the following equation:
 >>> a
 5(x^3/2) + 3x^2 + 11
 """
-def compute():
+def compute(num):
+  k = (num*num*num)/2
+  l = 5*k
+  m = num*num
+  n = 3*m
+  h = l+n+11
+  return h
+compute(x)
+"""
+>>> b
+"""
+def compute_1():
