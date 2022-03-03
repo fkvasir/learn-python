c82806aa4ba814c3b334736da32a724514005457
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2022-03-03 12:31:31 +0800

    added changes

--- .py
+++ .py
@@ -1,2 +1,14 @@
-w = ord("y")
-print(w)
+name = ["L","y","n","n","i","e"]
+w = ord(name[0])
+x = ord(name[1])
+y = ord(name[2])
+z = ord(name[3])
+
+def compute():
+  if x == x:
+    k = (x*x*x)/2
+    l = 5*float(k)
+    m = x*x
+    n = 3*float(m)
+    h = float(l)+float(n)+11
+    return h
