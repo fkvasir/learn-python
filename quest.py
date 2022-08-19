0d036dedaf897b5a7e9ac5416498ce4c1f99f7bd
fkvasir <fulgentkvasir.lavesores@g.msuiit.edu.ph>
2022-08-20 01:13:12 +0800

    added changes

--- quest.py
+++ quest.py
@@ -1,20 +1,21 @@
 from multiplechoice import question
 question_prompt =[
-  "What color are the apples?\n(a)Red\n(b)Pink\n(c)Yellow\n(d)Blue\n\n"
-  "What is your hobby?\n(a)Reading\n(b)Gaming\n(c)Sleeping\n\n"
+  "What color are the apples?\n(a)Red\n(b)Pink\n(c)Yellow\n(d)Blue\n\n",
+  "What is my hobby?\n(a)Sleeping\n(b)Gaming\n(c)Writing\n(d)Reading\n\n",
   
 ]
 
 questions = [
-  question(question_prompt[0], "a")
-
+  question(question_prompt[0], "a"),
+  question(question_prompt[1], "b"),
+  
 ]
 
 def run_test(questions):
   score = 0
   for question in questions:
-    answer1 = input(question.prompt)
-    if answer1 == input(question.answer):
+    answer = input(question.prompt)
+    if answer == input(question.answer):
       score += 1
-  print("You got" + str(score) + str(len(question)) + "right" +"Congratulations!!")
+  print("You got ~" + str(score) + "/"+ str(len(questions)) + "~ right>.<" +" Congratulations!!")
 run_test(questions)
